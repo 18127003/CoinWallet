307f1cb47634c672e45716f17b83940f665aeac5
18127003 <seadog150600@gmail.com>
2022-04-09 11:38:13 +0700

    add qr util

---
+++ app/src/main/java/me/app/coinwallet/utils/QRUtil.java
@@ -0,0 +1,103 @@
+package me.app.coinwallet.utils;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Hashtable;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class QRUtil {
+    private final static QRCodeWriter QR_CODE_WRITER = new QRCodeWriter();
+    private static final Logger log = LoggerFactory.getLogger(QRUtil.class);
+
+    public static Bitmap createQRCodeBitmap(final String content) {
+        try {
+            final Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
+            hints.put(EncodeHintType.MARGIN, 0);
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            final BitMatrix result = QR_CODE_WRITER.encode(content, BarcodeFormat.QR_CODE, 0, 0, hints);
+
+            final int width = result.getWidth();
+            final int height = result.getHeight();
+            final byte[] pixels = new byte[width * height];
+
+            for (int y = 0; y < height; y++) {
+                final int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    pixels[offset + x] = (byte) (result.get(x, y) ? -1 : 0);
+                }
+            }
+
+            final Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8);
+            bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(pixels));
+            return bitmap;
+        } catch (final WriterException x) {
+            log.info("problem creating qr code", x);
+            return null;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String encodeCompressBinary(final byte[] bytes) {
+        try {
+            final ByteArrayOutputStream bos = new ByteArrayOutputStream(bytes.length);
+            final GZIPOutputStream gos = new GZIPOutputStream(bos);
+            gos.write(bytes);
+            gos.close();
+
+            final byte[] gzippedBytes = bos.toByteArray();
+            final boolean useCompression = gzippedBytes.length < bytes.length;
+
+            final StringBuilder str = new StringBuilder();
+            str.append(useCompression ? 'Z' : '-');
+            str.append(Base43.encode(useCompression ? gzippedBytes : bytes));
+
+            return str.toString();
+        } catch (final IOException x) {
+            throw new RuntimeException(x);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String encodeBinary(final byte[] bytes) {
+        return Base43.encode(bytes);
+    }
+
+    public static byte[] decodeDecompressBinary(final String content) throws IOException {
+        final boolean useCompression = content.charAt(0) == 'Z';
+        final byte[] bytes = Base43.decode(content.substring(1));
+
+        InputStream is = new ByteArrayInputStream(bytes);
+        if (useCompression)
+            is = new GZIPInputStream(is);
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        final byte[] buf = new byte[4096];
+        int read;
+        while (-1 != (read = is.read(buf)))
+            baos.write(buf, 0, read);
+        baos.close();
+        is.close();
+
+        return baos.toByteArray();
+    }
+
+    public static byte[] decodeBinary(final String content) throws IOException {
+        return Base43.decode(content);
+    }
+}
