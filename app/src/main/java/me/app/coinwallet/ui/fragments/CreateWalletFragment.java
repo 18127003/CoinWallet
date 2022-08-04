391af3108e173de48b4858d1fe5cc482c963325e
18127016 <18127016@student.hcmus.edu.vn>
2022-08-04 15:27:06 +0700

    mndev

--- app/src/main/java/me/app/coinwallet/ui/fragments/CreateWalletFragment.java
+++ app/src/main/java/me/app/coinwallet/ui/fragments/CreateWalletFragment.java
@@ -3,6 +3,8 @@ package me.app.coinwallet.ui.fragments;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,9 @@ import android.view.ViewGroup;
 import com.google.android.material.textfield.TextInputEditText;
 import me.app.coinwallet.Constants;
 import me.app.coinwallet.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CreateWalletFragment extends Fragment {
 
@@ -43,6 +48,12 @@ public class CreateWalletFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         label = view.findViewById(R.id.label_text_field);
+
+        List<String> items = Arrays.asList(new String[]{"Bitcoin Mainnet", "Bitcoin Testnet", "Linecoin Testnet"});
+        ArrayAdapter adapter = new ArrayAdapter(requireContext(), R.layout.coin_select_item, items);
+        AutoCompleteTextView option= view.findViewById(R.id.select_option);
+        option.setAdapter(adapter);
+
         createBtn = view.findViewById(R.id.create_wallet_btn);
         createBtn.setOnClickListener(v->{
             Intent result = new Intent();
